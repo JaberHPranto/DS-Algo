64d12f21cdc3d4a896737c9984c954cabe6fd399
JaberHPranto <jhpranto53@gmail.com>
2021-11-05 22:21:01 +0600

    Initialization of the repo

---
+++ src/theory/data_structure/arrays/Arrays.java
@@ -0,0 +1,7 @@
+package theory.data_structure.arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/theory/data_structure/strings/Strings.java
@@ -0,0 +1,7 @@
+package theory.data_structure.strings;
+
+public class Strings {
+    public static void main(String[] args) {
+        System.out.println("This is for string");
+    }
+}
